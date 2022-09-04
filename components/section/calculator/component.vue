91d2e3fb5ec093baf547c346fd4ecb8f29a90006
ekimov_de <ekimov_de@mail.ru>
2022-09-05 00:45:57 +0700

    Доработки по сайту

--- components/section/calculator/component.vue
+++ components/section/calculator/component.vue
@@ -3,7 +3,7 @@
     id="calculator"
     :class="b()"
   >
-    <h2 :class="b('title')">
+    <h2>
       Честный калькулятор дохода
     </h2>
 
@@ -11,12 +11,167 @@
       Всего два правила. Честность и самодисциплина – два аспекта,
       которые помогут с первых дней начать зарабатывать.
     </p>
+
+    <div :class="b('wrapper')">
+      <div :class="b('content')">
+        <div :class="b('block')">
+          <p :class="b('text')">
+            Количество смен в неделю:
+          </p>
+
+          <ul :class="b('list')">
+            <li
+              v-for="(item, index) in shifts"
+              :key="index"
+            >
+              <input
+                :id="`shift #${index}`"
+                v-model="form.shift"
+                type="radio"
+                :value="item.value"
+              >
+              <label :for="`shift #${index}`">
+                {{ item.label }}
+              </label>
+            </li>
+          </ul>
+        </div>
+
+        <div :class="b('block')">
+          <p :class="b('text')">
+            Знание английского языка:
+          </p>
+
+          <ul :class="b('list')">
+            <li
+              v-for="(item, index) in languages"
+              :key="index"
+            >
+              <input
+                :id="`language #${index}`"
+                v-model="form.language"
+                type="radio"
+                :value="item.value"
+              >
+              <label :for="`language #${index}`">
+                {{ item.label }}
+              </label>
+            </li>
+          </ul>
+        </div>
+
+        <div :class="b('block')">
+          <p :class="b('text')">
+            Показать доход за:
+          </p>
+
+          <ul :class="b('list')">
+            <li
+              v-for="(item, index) in intervals"
+              :key="index"
+            >
+              <input
+                :id="`interval #${index}`"
+                v-model="form.interval"
+                type="radio"
+                :value="item.value"
+              >
+              <label :for="`interval #${index}`">
+                {{ item.label }}
+              </label>
+            </li>
+          </ul>
+        </div>
+      </div>
+
+      <div :class="b('inner')">
+        <h5 :class="b('title')">
+          Средняя зарплата:
+        </h5>
+
+        <div :class="b('inner-block')">
+          <h5 :class="b('amount')">
+            {{ displayedAmount }}
+          </h5>
+
+          <p :class="b('currency')">
+            руб.
+          </p>
+        </div>
+      </div>
+    </div>
   </section>
 </template>
 
 <script>
+  import { params } from './params';
+
   export default {
-    name: 'section-calculator'
+    name: 'section-calculator',
+    data() {
+      return {
+        form: {
+          shift: 0,
+          language: 0,
+          interval: 0
+        },
+        shifts: [
+          {
+            value: 0,
+            label: '5 смен'
+          },
+          {
+            value: 1,
+            label: '6 смен'
+          },
+          {
+            value: 2,
+            label: '7 смен'
+          }
+        ],
+        languages: [
+          {
+            value: 0,
+            label: 'начальный'
+          },
+          {
+            value: 1,
+            label: 'средний'
+          },
+          {
+            value: 2,
+            label: 'разговорный'
+          }
+        ],
+        intervals: [
+          {
+            value: 0,
+            label: 'день'
+          },
+          {
+            value: 1,
+            label: 'неделя'
+          },
+          {
+            value: 2,
+            label: 'месяц'
+          }
+        ]
+      };
+    },
+    computed: {
+      displayedAmount() {
+        const filteredParams = params
+          .filter(item => item.shift === this.form.shift)
+          .filter(item => item.language === this.form.language);
+
+        const currentParam = filteredParams[0].interval.find(item => {
+          return item.value === this.form.interval;
+        });
+
+        return currentParam.amount;
+      }
+    }
   };
 </script>
 
---
+++ components/section/calculator/params.js
@@ -0,0 +1,166 @@
+export const params = [
+  {
+    shift: 0,
+    language: 0,
+    interval: [
+      {
+        value: 0,
+        amount: 5000
+      },
+      {
+        value: 1,
+        amount: 24000
+      },
+      {
+        value: 2,
+        amount: 96000
+      }
+    ]
+  },
+  {
+    shift: 0,
+    language: 1,
+    interval: [
+      {
+        value: 0,
+        amount: 6500
+      },
+      {
+        value: 1,
+        amount: 32000
+      },
+      {
+        value: 2,
+        amount: 128000
+      }
+    ]
+  },
+  {
+    shift: 0,
+    language: 2,
+    interval: [
+      {
+        value: 0,
+        amount: 7200
+      },
+      {
+        value: 1,
+        amount: 36000
+      },
+      {
+        value: 2,
+        amount: 144000
+      }
+    ]
+  },
+
+  {
+    shift: 1,
+    language: 0,
+    interval: [
+      {
+        value: 0,
+        amount: 6000
+      },
+      {
+        value: 1,
+        amount: 29000
+      },
+      {
+        value: 2,
+        amount: 116000
+      }
+    ]
+  },
+  {
+    shift: 1,
+    language: 1,
+    interval: [
+      {
+        value: 0,
+        amount: 7500
+      },
+      {
+        value: 1,
+        amount: 38000
+      },
+      {
+        value: 2,
+        amount: 152000
+      }
+    ]
+  },
+  {
+    shift: 1,
+    language: 2,
+    interval: [
+      {
+        value: 0,
+        amount: 8200
+      },
+      {
+        value: 1,
+        amount: 43000
+      },
+      {
+        value: 2,
+        amount: 173000
+      }
+    ]
+  },
+
+  {
+    shift: 2,
+    language: 0,
+    interval: [
+      {
+        value: 0,
+        amount: 7000
+      },
+      {
+        value: 1,
+        amount: 34000
+      },
+      {
+        value: 2,
+        amount: 136000
+      }
+    ]
+  },
+  {
+    shift: 2,
+    language: 1,
+    interval: [
+      {
+        value: 0,
+        amount: 8500
+      },
+      {
+        value: 1,
+        amount: 45000
+      },
+      {
+        value: 2,
+        amount: 180000
+      }
+    ]
+  },
+  {
+    shift: 2,
+    language: 2,
+    interval: [
+      {
+        value: 0,
+        amount: 9200
+      },
+      {
+        value: 1,
+        amount: 51000
+      },
+      {
+        value: 2,
+        amount: 204000
+      }
+    ]
+  }
+];
