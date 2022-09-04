080adc624cd01f8d67bd0972ee60e8a85399f317
ekimov_de <ekimov_de@mail.ru>
2022-09-04 21:52:39 +0700

    Настройка конфига

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,31 +1,39 @@
 export default {
   target: 'static',
 
+  buildDir: 'dist',
+
+  ssr: false,
+
+  generate: {
+    fallback: true
+  },
+
   head: {
     title: 'flats-studio',
     htmlAttrs: {
-      lang: 'en',
+      lang: 'en'
     },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' },
+      { name: 'format-detection', content: 'telephone=no' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
       { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Quicksand:wght@300;400;500;600&display=swap' }
-    ],
+    ]
   },
 
   css: [
-    '~/assets/styles/main.scss',
+    '~/assets/styles/main.scss'
   ],
 
   styleResources: {
     scss: [
-     '~/assets/styles/main.scss',
-    ],
+     '~/assets/styles/main.scss'
+    ]
    },
 
   plugins: [
@@ -36,12 +44,12 @@ export default {
 
   buildModules: [
     '@nuxtjs/eslint-module',
-    '@nuxtjs/stylelint-module',
+    '@nuxtjs/stylelint-module'
   ],
 
   modules: [
     '@nuxtjs/style-resources'
   ],
 
-  build: {},
-}
+  build: {}
+};
